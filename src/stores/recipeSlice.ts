f04b37c5bce55b8e006d56d44333f6b1ce78865e
Guchito <a_gaggero@hotmail.com>
2024-05-18 16:05:02 +0200

    Getting the recipe details

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,5 +1,5 @@
 import { StateCreator } from "zustand"
-import { getCategories, getRecipes } from "../services/RecipeService"
+import { getCategories, getRecipeById, getRecipes } from "../services/RecipeService"
 import type { Categories,Drink , Drinks, SearchFilter } from "../types"
 
 export type RecipesSliceType = {
@@ -31,6 +31,7 @@ export const createRecipesSlices : StateCreator<RecipesSliceType> = (set) => ({
         })
     },
     selectRecipe: async (id) => {
-        console.log(id)
+        const selectedRecipe = await getRecipeById(id)
+        console.log(selectedRecipe)
     }
 })
